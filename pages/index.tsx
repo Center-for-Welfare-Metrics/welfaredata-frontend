212759477ccca433ad9dccc485d127db17e2a4f2
Herikle Mesquita <herikle.mesquita@gmail.com>
2021-01-14 11:51:40 -0300

    redirect to login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link'
 import onlyGuest from '@/components/HOC/onlyGuest'
+import Router from 'next/router'
+import { useEffect } from 'react'
 
 const Home = () => {
+
+  useEffect(()=>{
+    Router.push('/login')
+  },[])
+
   return (
     <div>
       <Link href='/login'>Login</Link>
