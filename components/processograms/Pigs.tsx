5935a9658a43b5821610b72cab51e2be0cb52cb5
Herikle Mesquita <herikle.mesquita@gmail.com>
2021-02-05 13:21:21 -0300

    fix import

--- components/processograms/Pigs.tsx
+++ components/processograms/Pigs.tsx
@@ -2,7 +2,6 @@ import Processogram from "./processogram"
 import {Container} from './zoo-styled'
 import ProcessogramContext from '@/context/processogram'
 import { useEffect, useRef, useState } from "react"
-import { TweenLite } from 'gsap'
 
 
 const svg_file_names = ['enhanced intensive.svg','european intensive.svg','outdoor semi-intensive.svg','conventional cages.svg']
