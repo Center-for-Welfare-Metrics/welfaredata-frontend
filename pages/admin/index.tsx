b27ea6f337a449993395452c126e422867a5b246
Herikle Mesquita <herikle.mesquita@gmail.com>
2021-02-11 20:38:43 -0300

    last commit on main branch || no relevant alterations

---
+++ pages/admin/index.tsx
@@ -0,0 +1,13 @@
+import DefaultLayout from "@/components/layouts"
+
+
+const AdminPage = () => {
+    return (
+        <DefaultLayout>
+            
+        </DefaultLayout>
+    )
+}
+
+
+export default AdminPage
