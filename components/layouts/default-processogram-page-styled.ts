0a372429431d4e9992156de39014809b2315e912
Herikle Mesquita <herikle.mesquita@gmail.com>
2021-06-03 10:02:19 -0300

    scroll fix

--- components/layouts/default-processogram-page-styled.ts
+++ components/layouts/default-processogram-page-styled.ts
@@ -1,9 +1,25 @@
-import styled from 'styled-components'
+import { lighten } from 'polished'
+import styled,{css} from 'styled-components'
 
-
+const scrollBar = (color,size='.5rem') => css`
+    ::-webkit-scrollbar {
+        width: ${size};
+        height: ${size};
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: ${({theme})=>theme.colors[color]};
+        transition: background-color 500ms;
+        border-radius:2rem;
+    }
+    ::-webkit-scrollbar-thumb:hover{
+        background-color: ${({theme})=> lighten(0.1,theme.colors[color])};
+        transition: background-color 500ms;
+    }
+`
 
 export const Container = styled.div`
     height:${({full}) => full?'100vh':'90vh'};    
     overflow:auto;    
-    position:relative;    
+    position:relative;  
+    ${scrollBar('gray')}  
 `
