dae37742aa89bbb1d246ef3776a2b5742fc8c2d1
Herikle Mesquita <herikle.mesquita@gmail.com>
2021-07-27 16:28:39 -0300

    fix(processogram): processogram margin was wrong

--- components/processograms/processogram-styled.ts
+++ components/processograms/processogram-styled.ts
@@ -3,8 +3,11 @@ import styled, {css} from 'styled-components'
 export const SvgContainer = styled.div`       
     width:100%;
     margin:2.5rem 0;
-    :first-child,:last-child{
-        margin:5rem 0;
+    :first-child{
+        margin-top:5rem;        
+    }
+    :last-child{
+        margin-bottom:5rem;        
     }
     >svg{        
         height:auto;            
