ed4c55f65d5a324ed31f893eb4cd749201eee00e
Herikle Mesquita <herikle.mesquita@gmail.com>
2022-09-07 21:20:44 -0300

    fix feedback form1

--- components/common/inputs/form-input-styled.ts
+++ components/common/inputs/form-input-styled.ts
@@ -1,39 +1,53 @@
-import { lighten, transparentize } from 'polished'
-import styled,{css} from 'styled-components'
+import { lighten, transparentize } from "polished";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
-    padding:2rem 0 2rem 0;
-    position: relative;
-    display:flex;
-    flex-direction: column;
-    box-sizing:border-box;
-    width:100%;
-`
+  padding: 2rem 0 2rem 0;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 100%;
+`;
 
 export const Icon = styled.i`
-    position:absolute;
-    top:2.5rem;
-    left:2rem;
-    color:${({theme})=>theme.colors.yellow};
-    font-size:${({theme})=>theme.fontSize.large};
-`
+  position: absolute;
+  top: 2.5rem;
+  left: 2rem;
+  color: ${({ theme }) => theme.colors.yellow};
+  font-size: ${({ theme }) => theme.fontSize.large};
+`;
 
-export const Label = styled.label`
-    color:${({theme})=>transparentize(0.5,theme.colors.blue)};
-    position:absolute;
-    white-space: nowrap;
-    ${({theme }) => css`
-        bottom:0;
-        transform:translateY(-50%);
-        z-index:auto;
-        font-size:${theme.fontSize.small};
-        color:${({theme})=>theme.colors.blue};
+type LabelProps = {
+  $hasValue: boolean;
+  $multiline: boolean;
+};
+
+export const Label = styled.label<LabelProps>`
+  color: ${({ theme }) => transparentize(0.5, theme.colors.blue)};
+  position: absolute;
+  white-space: nowrap;
+  ${({ theme }) => css`
+    bottom: 0;
+    transform: translateY(-50%);
+    z-index: auto;
+    font-size: ${theme.fontSize.small};
+    color: ${({ theme }) => theme.colors.blue};
+  `}
+
+  ${({ $hasValue, $multiline }) =>
+    $hasValue &&
+    !$multiline &&
+    css`
+      transform: translateY(-150%);
     `}
-`
+
+    transition: transform 100ms linear;
+`;
 export const Error = styled.span`
-    font-size:${({theme})=>theme.fontSize.small};
-    color:${({theme})=>theme.colors.red};
-    white-space: pre-wrap;
-    text-align: center;
-    font-weight:bold;
-`
+  font-size: ${({ theme }) => theme.fontSize.small};
+  color: ${({ theme }) => theme.colors.red};
+  white-space: pre-wrap;
+  text-align: center;
+  font-weight: bold;
+`;
--- components/common/inputs/form-input.tsx
+++ components/common/inputs/form-input.tsx
@@ -1,73 +1,73 @@
-import { useState,FC,InputHTMLAttributes, useEffect, TextareaHTMLAttributes } from 'react'
+import {
+  useState,
+  FC,
+  InputHTMLAttributes,
+  useEffect,
+  TextareaHTMLAttributes,
+} from "react";
 
-import {Container,Label,Icon,Error} from './form-input-styled'
-import { LabeledInput, LabeledTextArea } from './inputs'
-import React from 'react'
+import { Container, Label, Icon, Error } from "./form-input-styled";
+import { LabeledInput, LabeledTextArea } from "./inputs";
+import React from "react";
 
 export interface IFormInput extends InputHTMLAttributes<HTMLInputElement> {
-    name:string,
-    type?:string,
-    error?:string,
-    label:string,
-    icon?:string,
-    value:any,
-    multiline?:boolean
-    disabled?:boolean
-    defaultValue?:any
-    customStyle?:any
-    minRows?:number
-    maxRows?:number
+  name: string;
+  type?: string;
+  error?: string;
+  label: string;
+  icon?: string;
+  value: any;
+  multiline?: boolean;
+  disabled?: boolean;
+  defaultValue?: any;
+  customStyle?: any;
+  minRows?: number;
+  maxRows?: number;
 }
 
-const FormInput : FC<IFormInput> | FC<TextareaHTMLAttributes<HTMLTextAreaElement>> = (
-    {
-        name,
-        multiline=false,
-        type='text',
-        error,
-        label,
-        icon,
-        value,
-        disabled=false,
-        defaultValue=null,
-        customStyle,        
-        ...rest
-    }
-) => {
+const FormInput:
+  | FC<IFormInput>
+  | FC<TextareaHTMLAttributes<HTMLTextAreaElement>> = ({
+  name,
+  multiline = false,
+  type = "text",
+  error,
+  label,
+  icon,
+  value,
+  disabled = false,
+  defaultValue = null,
+  customStyle,
+  ...rest
+}) => {
+  return (
+    <Container style={customStyle}>
+      <Label $multiline={multiline} $hasValue={!!value} htmlFor={name}>
+        {label}
+      </Label>
+      {multiline ? (
+        <LabeledTextArea
+          id={name}
+          name={name}
+          type={type}
+          value={value}
+          disabled={disabled}
+          {...rest}
+        />
+      ) : (
+        <LabeledInput
+          id={name}
+          name={name}
+          type={type}
+          value={value}
+          disabled={disabled}
+          {...rest}
+        />
+      )}
 
-    return (
-        <Container style={customStyle}>
-            <Label htmlFor={name}>{label}</Label>
-            {
-                multiline?
-                (
-                    <LabeledTextArea 
-                        id={name}
-                        name={name}
-                        type={type}
-                        value={value}
-                        disabled={disabled}                        
-                        {...rest}
-                    />
-                )
-                :
-                (
-                    <LabeledInput 
-                        id={name}
-                        name={name}
-                        type={type}
-                        value={value}
-                        disabled={disabled}
-                        {...rest}                        
-                    />
-                )
-            }
-            
-            {error && <Error>
-                {error}
-            </Error>}
-        </Container>
-    )
-}
+      {error && <Error>{error}</Error>}
+    </Container>
+  );
+};
 
-export default React.memo(FormInput)
+export default React.memo(FormInput);
