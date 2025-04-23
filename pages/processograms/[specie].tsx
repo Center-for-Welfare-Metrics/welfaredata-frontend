a388a1fad8a441954a54517e5097b84d5d84a008
Herikle Mesquita <herikle.mesquita@gmail.com>
2025-04-23 10:52:48 -0300

    remove base64 conversion

--- pages/processograms/[specie].tsx
+++ pages/processograms/[specie].tsx
@@ -45,54 +45,54 @@ export async function getStaticProps(context: GetStaticPropsContext) {
 
   let elements: Element[] = await (await publicApi.getElements(specie)).data;
 
-  const MAX_IMAGE_SIZE_BYTES = 2 * 1024 * 1024; // 2MB
+  //const MAX_IMAGE_SIZE_BYTES = 2 * 1024 * 1024; // 2MB
 
-  console.log("Converting images to base64...");
+  // console.log("Converting images to base64...");
 
-  const elementsWithBase64 = await Promise.all(
-    elements.map(async (el) => {
-      const newRasterImages: Element["raster_images"] = {};
+  // const elementsWithBase64 = await Promise.all(
+  //   elements.map(async (el) => {
+  //     const newRasterImages: Element["raster_images"] = {};
 
-      for (const [key, img] of Object.entries(el.raster_images)) {
-        try {
-          const res = await fetch(img.src);
-          const arrayBuffer = await res.arrayBuffer();
-          const buffer = Buffer.from(arrayBuffer);
+  //     for (const [key, img] of Object.entries(el.raster_images)) {
+  //       try {
+  //         const res = await fetch(img.src);
+  //         const arrayBuffer = await res.arrayBuffer();
+  //         const buffer = Buffer.from(arrayBuffer);
 
-          if (buffer.length > MAX_IMAGE_SIZE_BYTES) {
-            console.warn(
-              `Imagem ignorada por ser muito grande (${(
-                buffer.length /
-                1024 /
-                1024
-              ).toFixed(2)} MB):`,
-              img.src
-            );
-            newRasterImages[key] = img; // mantém o src original
-            continue;
-          }
+  //         if (buffer.length > MAX_IMAGE_SIZE_BYTES) {
+  //           console.warn(
+  //             `Imagem ignorada por ser muito grande (${(
+  //               buffer.length /
+  //               1024 /
+  //               1024
+  //             ).toFixed(2)} MB):`,
+  //             img.src
+  //           );
+  //           newRasterImages[key] = img; // mantém o src original
+  //           continue;
+  //         }
 
-          const base64 = `data:image/png;base64,${buffer.toString("base64")}`;
-          newRasterImages[key] = {
-            ...img,
-            src: base64,
-          };
-        } catch (error) {
-          console.error("Erro ao converter imagem:", img.src, error);
-          newRasterImages[key] = img; // fallback pro original
-        }
-      }
+  //         const base64 = `data:image/png;base64,${buffer.toString("base64")}`;
+  //         newRasterImages[key] = {
+  //           ...img,
+  //           src: base64,
+  //         };
+  //       } catch (error) {
+  //         console.error("Erro ao converter imagem:", img.src, error);
+  //         newRasterImages[key] = img; // fallback pro original
+  //       }
+  //     }
 
-      return {
-        ...el,
-        raster_images: newRasterImages,
-      };
-    })
-  );
+  //     return {
+  //       ...el,
+  //       raster_images: newRasterImages,
+  //     };
+  //   })
+  // );
 
   return {
     props: {
-      elements: elementsWithBase64,
+      elements: elements,
       specie,
     },
   };
